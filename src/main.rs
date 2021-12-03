f9b4cd4c10fa69bc4b33807e94481613f36bab80
Bjørn Madsen <bm@lego.com>
2021-12-03 14:05:14 +0100

    Solve day 3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![allow(unused)]
 
+use anyhow::Result;
 use std::fmt::Display;
 use std::fs::read_to_string;
-use anyhow::Result;
 
 fn main() {
-    day2::part2();
+    day3::part2();
 }
 
 fn load_raw<T, F>(day: usize, f: F) -> Vec<T>
-    where F: FnMut(&str) -> T {
+where
+    F: FnMut(&str) -> T,
+{
     read_to_string(format!("data/{:02}.txt", day))
         .expect(format!("Could not find data file for day {}", day).as_str())
         .trim()
@@ -24,14 +26,23 @@ fn load_strings(day: usize) -> Vec<String> {
 }
 
 fn load_ints(day: usize) -> Vec<i32> {
-    load_raw(day, |s| s.parse::<i32>().expect(format!("Failed to parse '{}' as an integer", s).as_str()))
+    load_raw(day, |s| {
+        s.parse::<i32>()
+            .expect(format!("Failed to parse '{}' as an integer", s).as_str())
+    })
+}
+
+fn load_radix_2(day: usize) -> Vec<usize> {
+    load_raw(day, |s| {
+        usize::from_str_radix(s, 2)
+            .expect(format!("Failed to parse '{}' as a binary number", s).as_str())
+    })
 }
 
 fn print_answer<T: Display>(day: usize, part: usize, answer: T) {
     println!("Day {:2}, part {}: {}", day, part, answer)
 }
 
-// Day 1
 mod day1 {
     use crate::*;
 
@@ -50,16 +61,19 @@ mod day1 {
     }
 
     pub fn part2() {
-        let data: Vec<i32> = load_ints(1).as_slice().windows(3).map(|w| w.iter().sum()).collect();
+        let data: Vec<i32> = load_ints(1)
+            .as_slice()
+            .windows(3)
+            .map(|w| w.iter().sum())
+            .collect();
         calc(2, data);
     }
 }
 
-// Day 2
 mod day2 {
+    use crate::*;
     use std::num::ParseIntError;
     use std::str::{FromStr, SplitWhitespace};
-    use crate::*;
 
     #[derive(Debug)]
     enum Direction {
@@ -72,21 +86,25 @@ mod day2 {
         type Err = anyhow::Error;
 
         fn from_str(s: &str) -> Result<Direction> {
-            let parse = |n: &str, f: fn(usize) -> Direction|
-                n.parse::<usize>().map(f).map_err(anyhow::Error::from);
+            let parse = |n: &str, f: fn(usize) -> Direction| {
+                n.parse::<usize>().map(f).map_err(anyhow::Error::from)
+            };
 
             let parts: Vec<&str> = s.split_whitespace().collect();
             match parts.as_slice() {
                 ["forward", n] => parse(n, Direction::Forward),
                 ["down", n] => parse(n, Direction::Down),
                 ["up", n] => parse(n, Direction::Up),
-                _ => Err(anyhow::Error::msg("Failed to parse direction"))
+                _ => Err(anyhow::Error::msg("Failed to parse direction")),
             }
         }
     }
 
     fn load_directions() -> Vec<Direction> {
-        load_strings(2).iter().map(|l| l.parse::<Direction>().unwrap()).collect()
+        load_strings(2)
+            .iter()
+            .map(|l| l.parse::<Direction>().unwrap())
+            .collect()
     }
 
     pub fn part1() {
@@ -121,5 +139,80 @@ mod day2 {
         }
 
         print_answer(2, 2, horiz * depth)
+    }
+}
+
+mod day3 {
+    use crate::*;
+
+    fn is_bit_set(n: &usize, bit: usize) -> bool {
+        *n & (1 << bit) != 0
+    }
+
+    fn filter_bits_set(numbers: Vec<usize>, bit: usize, is_set: bool) -> Vec<usize> {
+        numbers
+            .into_iter()
+            .filter(move |n| is_bit_set(n, bit) == is_set)
+            .collect()
+    }
+
+    pub fn part1() {
+        let data = load_radix_2(3);
+        let data_len = data.len();
+
+        let mut bit_count = [0; 12];
+
+        for mut number in data {
+            for i in (0..12).rev() {
+                bit_count[i] += number & 1;
+                number >>= 1;
+            }
+        }
+
+        let mut gamma_rate: usize = 0;
+        for bit in bit_count {
+            gamma_rate <<= 1;
+            if bit > data_len / 2 {
+                gamma_rate |= 1;
+            }
+        }
+
+        let epsilon_rate = !gamma_rate & 0b1111_1111_1111;
+
+        print_answer(3, 1, gamma_rate * epsilon_rate);
+    }
+
+    fn filter(i: usize, numbers: Vec<usize>, choose_most_common: bool) -> usize {
+        let filtered_set = filter_bits_set(numbers.clone(), i, true);
+        let bits_set = filtered_set.len();
+        let bits_unset = numbers.len() - bits_set;
+
+        let should_bit_be_set = match (choose_most_common, bits_set, bits_unset) {
+            (true, s, u) if s >= u => true,
+            (true, _, _) => false,
+            (false, s, u) if s >= u => false,
+            (false, _, _) => true,
+        };
+
+        let filtered = if should_bit_be_set {
+            filtered_set
+        } else {
+            filter_bits_set(numbers, i, false)
+        };
+
+        match filtered.as_slice() {
+            [n] => *n,
+            _ => filter(i - 1, filtered, choose_most_common),
+        }
+    }
+
+    pub fn part2() {
+        let data = load_radix_2(3);
+        let data_len = data.len();
+
+        let oxy_gen_rating = filter(11, data.clone(), true);
+        let co2_scrub_rating = filter(11, data, false);
+
+        print_answer(3, 2, oxy_gen_rating * co2_scrub_rating);
     }
 }
