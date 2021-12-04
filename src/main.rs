4c599c7ff9f02b45a7d2dfba48a86be6448b0ddd
Bjørn Madsen <bm@lego.com>
2021-12-04 23:06:46 +0100

    Refactor data loading

--- src/main.rs
+++ src/main.rs
@@ -3,75 +3,61 @@
 use anyhow::Result;
 use std::fmt::Display;
 use std::fs::read_to_string;
+use std::str::FromStr;
 
 fn main() {
     day3::part2();
 }
 
-fn load_raw<T, F>(day: usize, f: F) -> Vec<T>
+fn load<T>(day: usize) -> Result<Vec<T>>
 where
-    F: FnMut(&str) -> T,
+    T: FromStr,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
 {
-    read_to_string(format!("data/{:02}.txt", day))
-        .expect(format!("Could not find data file for day {}", day).as_str())
-        .trim()
-        .lines()
-        .filter(|l| !l.is_empty())
-        .map(f)
+    let raw_data = read_to_string(format!("data/{:02}.txt", day))?;
+    let lines = raw_data.trim().lines();
+    lines
+        .map(|line| line.parse().map_err(anyhow::Error::from))
         .collect()
 }
 
-fn load_strings(day: usize) -> Vec<String> {
-    load_raw(day, |s| s.to_owned())
+fn print_answer<T: Display>(day: usize, part: usize, answer: T) -> Result<()> {
+    Ok(println!("Day {:2}, part {}: {}", day, part, answer))
 }
 
-fn load_ints(day: usize) -> Vec<i32> {
-    load_raw(day, |s| {
-        s.parse::<i32>()
-            .expect(format!("Failed to parse '{}' as an integer", s).as_str())
-    })
-}
-
-fn load_radix_2(day: usize) -> Vec<usize> {
-    load_raw(day, |s| {
-        usize::from_str_radix(s, 2)
-            .expect(format!("Failed to parse '{}' as a binary number", s).as_str())
-    })
-}
-
-fn print_answer<T: Display>(day: usize, part: usize, answer: T) {
-    println!("Day {:2}, part {}: {}", day, part, answer)
-}
-
+//<editor-fold desc="Day 1">
 mod day1 {
     use crate::*;
 
-    fn calc(part: usize, data: Vec<i32>) {
-        let answer = data
-            .iter()
+    fn calc(part: usize, data: Vec<i32>) -> usize {
+        data.iter()
             .zip(data.iter().skip(1))
             .filter(|(a, b)| b > a)
-            .count();
-
-        print_answer(1, part, answer);
+            .count()
     }
 
-    pub fn part1() {
-        calc(1, load_ints(1));
+    pub fn part1() -> Result<()> {
+        let answer = calc(1, load(1)?);
+
+        print_answer(1, 1, answer)
     }
 
-    pub fn part2() {
-        let data: Vec<i32> = load_ints(1)
+    pub fn part2() -> Result<()> {
+        let data: Vec<i32> = load(1)?
             .as_slice()
             .windows(3)
             .map(|w| w.iter().sum())
             .collect();
-        calc(2, data);
+        let answer = calc(2, data);
+        print_answer(1, 2, answer)
     }
 }
+//</editor-fold>
 
+//<editor-fold desc="Day 2">
 mod day2 {
     use crate::*;
+    use std::fmt::{Debug, Formatter};
     use std::num::ParseIntError;
     use std::str::{FromStr, SplitWhitespace};
 
@@ -82,12 +68,18 @@ mod day2 {
         Up(usize),
     }
 
-    impl FromStr for Direction {
-        type Err = anyhow::Error;
+    #[derive(Debug, thiserror::Error)]
+    #[error("{0}")]
+    pub struct DirectionParseError(String);
 
-        fn from_str(s: &str) -> Result<Direction> {
+    impl FromStr for Direction {
+        type Err = DirectionParseError;
+
+        fn from_str(s: &str) -> Result<Direction, Self::Err> {
             let parse = |n: &str, f: fn(usize) -> Direction| {
-                n.parse::<usize>().map(f).map_err(anyhow::Error::from)
+                n.parse::<usize>()
+                    .map(f)
+                    .map_err(|err| DirectionParseError(err.to_string()))
             };
 
             let parts: Vec<&str> = s.split_whitespace().collect();
@@ -95,23 +87,16 @@ mod day2 {
                 ["forward", n] => parse(n, Direction::Forward),
                 ["down", n] => parse(n, Direction::Down),
                 ["up", n] => parse(n, Direction::Up),
-                _ => Err(anyhow::Error::msg("Failed to parse direction")),
+                _ => Err(DirectionParseError("failed to parse direction".to_owned())),
             }
         }
     }
 
-    fn load_directions() -> Vec<Direction> {
-        load_strings(2)
-            .iter()
-            .map(|l| l.parse::<Direction>().unwrap())
-            .collect()
-    }
-
-    pub fn part1() {
+    pub fn part1() -> Result<()> {
         let mut horiz = 0;
         let mut depth = 0;
 
-        for direction in load_directions() {
+        for direction in load(2)? {
             match direction {
                 Direction::Forward(n) => horiz += n,
                 Direction::Down(n) => depth += n,
@@ -122,12 +107,12 @@ mod day2 {
         print_answer(2, 1, horiz * depth)
     }
 
-    pub fn part2() {
+    pub fn part2() -> Result<()> {
         let mut horiz = 0;
         let mut depth = 0;
         let mut aim = 0;
 
-        for direction in load_directions() {
+        for direction in load::<Direction>(2)? {
             match direction {
                 Direction::Forward(n) => {
                     horiz += n;
@@ -141,9 +126,18 @@ mod day2 {
         print_answer(2, 2, horiz * depth)
     }
 }
+//</editor-fold>
 
+//<editor-fold desc="Day 3">
 mod day3 {
     use crate::*;
+
+    fn load() -> Result<Vec<usize>> {
+        super::load::<String>(3)?
+            .iter()
+            .map(|s| usize::from_str_radix(s, 2).map_err(anyhow::Error::from))
+            .collect()
+    }
 
     fn is_bit_set(n: &usize, bit: usize) -> bool {
         *n & (1 << bit) != 0
@@ -156,8 +150,8 @@ mod day3 {
             .collect()
     }
 
-    pub fn part1() {
-        let data = load_radix_2(3);
+    pub fn part1() -> Result<()> {
+        let data = load()?;
         let data_len = data.len();
 
         let mut bit_count = [0; 12];
@@ -179,7 +173,7 @@ mod day3 {
 
         let epsilon_rate = !gamma_rate & 0b1111_1111_1111;
 
-        print_answer(3, 1, gamma_rate * epsilon_rate);
+        print_answer(3, 1, gamma_rate * epsilon_rate)
     }
 
     fn filter(i: usize, numbers: Vec<usize>, choose_most_common: bool) -> usize {
@@ -206,13 +200,14 @@ mod day3 {
         }
     }
 
-    pub fn part2() {
-        let data = load_radix_2(3);
+    pub fn part2() -> Result<()> {
+        let data = load()?;
         let data_len = data.len();
 
         let oxy_gen_rating = filter(11, data.clone(), true);
         let co2_scrub_rating = filter(11, data, false);
 
-        print_answer(3, 2, oxy_gen_rating * co2_scrub_rating);
+        print_answer(3, 2, oxy_gen_rating * co2_scrub_rating)
     }
 }
+//</editor-fold>
